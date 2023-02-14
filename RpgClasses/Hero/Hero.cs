c37d6f91abe1ce163dfdd613b6d10a46ab977c01
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-02-14 14:02:25 +0100

    EquipFunction
    Trying to make equipment logic work. Not sure how to cast an equipped item to either armor or weapon on equipping it

--- RpgClasses/Hero/Hero.cs
+++ RpgClasses/Hero/Hero.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections;
 
 public class Hero
 {
     string heroName;
     int level = 1;
     public HeroAttribute levelAttributes;
-    Item[] equipment;
+    public Hashtable equipment;
+    public enum Slots 
+    {
+        Weapon = 1,
+        Head = 2,
+        Body = 3,
+        Legs = 4
+    } 
 
     public Hero()
 	{
@@ -19,7 +27,22 @@ public class Hero
     }
     public void equipItem(Item item) 
     {
+        if(item.GetType() == typeof(Weapon)) 
+        { 
+            equipWeapon(item);
+        }
+        else if(item.GetType() == typeof(Armor)) 
+        {
+            equipArmor(item);
+        }
+    }
+    public virtual void equipWeapon(Item weapon) 
+    {
         
+    }
+    public virtual void equipArmor(Item armor) 
+    {
+
     }
     public void setName(string name) 
     {
@@ -28,6 +51,20 @@ public class Hero
     public string getName()
     {
         return heroName;
+    }
+
+    public int getLevel() 
+    {
+        return level;
+    }
+    public void initializeEquipment() 
+    {
+        equipment = new Hashtable();
+        equipment.Add(Slots.Weapon, null);
+        equipment.Add(Slots.Head, null);
+        equipment.Add(Slots.Body, null);
+        equipment.Add(Slots.Legs, null);
+        
     }
 
 }
--- RpgClasses/Hero/Mage.cs
+++ RpgClasses/Hero/Mage.cs
@@ -2,11 +2,22 @@
 
 public class Mage : Hero
 {
+	public enum ValidWeaponTypes
+	{
+		Staff = 5,
+		Wand = 7
+	}
+	public enum ValidArmorTypes
+	{
+		Cloth = 1
+	}
+
 	public Mage(string heroName)
 	{
 		int[] startingAttributes = new int[3] {1,1,8};
 		int[] levelUpAttributes = new int[3] { 1, 1, 5 };
 		levelAttributes = new HeroAttribute(startingAttributes, levelUpAttributes);
 		setName(heroName);
+		initializeEquipment();
 	}
 }
